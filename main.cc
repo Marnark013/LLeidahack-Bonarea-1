94c0b9cf130fa2bad1bfe99ef5c7310d24852aff
Marnark013 <marcolleo213@gmail.com>
2023-11-25 18:33:55 +0100

    a mitjes

--- main.cc
+++ main.cc
@@ -6,6 +6,7 @@
 #include <map>
 #include <iomanip>
 #include <ctime>
+#include <queue>
 
 using VI = std::vector<int>;
 using VVI = std::vector<VI>;
@@ -19,6 +20,12 @@ using VS = std::vector <std::string>;
 
 VVS boardInfo = VVS(20, VS(47, ""));
 VVI passableBoard = VVI(20, VI(47, true));
+
+std::vector<std::pair<int, int>> dirs = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+
+enum Dir {
+    Down, Right, Up, Left, None
+};
 
 struct TicketInfo
 {
@@ -34,21 +41,25 @@ MSTI clientsInfo; //client id to his ticket info
 MTS ordClients; //clients id ordered by the time they enter the store
 
 struct Pos {
-    int x = -1;
-    int y = -1;
+    int x;
+    int y;
 
     Pos(int px, int py) : x(px), y(py) {}
+    Pos() : x(-1), y(-1) {}
 };
 
 struct itemInfo {
     VI pickingTimes;
     std::string itemName;
+    Pos pos;
 
-    itemInfo() : pickingTimes(VI(5, 0)), itemName("") {}
-    itemInfo(std::string itemname, VI temps) : pickingTimes(temps), itemName(itemname) {}
+    itemInfo() : pickingTimes(VI(5, 0)), itemName(""), pos(-1, -1) {}
+    itemInfo(std::string itemname, VI temps, Pos p) : pickingTimes(temps), itemName(itemname), pos(p) {}
 };
 
 std::map<std::string, itemInfo> itemInfoMap;
+std::map<std::string, std::map<std::string, int>> distancesMap;
+std::map<std::string, int> testMap;
 
 //posa a boardInfo id de producte quan hi ha producte (picking_pos) i false als llocs on no es pot pathear de passableBoard
 void readBoard(const std::string& filename) {
@@ -83,6 +94,7 @@ void readBoard(const std::string& filename) {
             std::getline(linestream, tmp, ';'); //agafa pickingY
             pickingY = stoi(tmp);
             boardInfo[pickingY-1][pickingX-1] = description;
+            itemInfoMap[description] = itemInfo("", VI(5, 0), Pos(pickingX-1, pickingY-1));
         }
     }
     file.close();
@@ -108,7 +120,8 @@ void readArticleInfo(const std::string& filename) {
             int tmp2 = stoi(tmp);
             v[i] = tmp2;
         }
-        itemInfoMap[itemId] = itemInfo(itemName, v);
+        Pos pos = itemInfoMap[itemId].pos;
+        itemInfoMap[itemId] = itemInfo(itemName, v, pos);
     }
     file.close();   
 }
@@ -159,7 +172,93 @@ void ReadTickets()
     }
 }
 
+bool pos_ok(int x, int y) {
+    return ((x >= 0 and x < 47) and (y < 20 and y >= 0));
+}
+
+void fillDistances(std::string itemId) {
+    VVB visited(20, VB(47, false));
+    VVI distances(20, VI(47, 0));
+    std::queue<Pos> Q;
+    Pos pos = itemInfoMap[itemId].pos;
+
+    Q.push(pos);
+    
+    while(not Q.empty()) {
+        Pos actPos = Q.front();
+        Q.pop();
+
+        if(boardInfo[actPos.y][actPos.x] != "") {
+            distancesMap[itemId][boardInfo[actPos.y][actPos.x]] = distances[actPos.y][actPos.y];
+        }
+        visited[actPos.y][actPos.x] = true;
+        for(int i = 0; i < 4; ++i) {
+            std::pair<int, int> dir = dirs[i];
+            Pos nwPos;
+            nwPos.x = actPos.x + dir.first;
+            nwPos.y = actPos.y + dir.second;
+            if(pos_ok(nwPos.x, nwPos.y) and passableBoard[nwPos.y][nwPos.x] and not visited[nwPos.y][nwPos.x]) {
+                Q.push(nwPos);
+                distances[nwPos.y][nwPos.x] = distances[actPos.y][actPos.y] + 1;
+                visited[nwPos.y][nwPos.x] = true;
+            }
+        }
+    }
+}
+
+void calculateDistances() {
+    for(int i = 0; i < 20; ++i) {
+        for(int j = 0; j < 47; ++j) {
+            if(boardInfo[i][j] != "") {
+                
+                fillDistances(boardInfo[i][j]);
+            }
+        }
+    }
+}
+
+void newFill(std::string id) {
+    Pos pos = itemInfoMap[id].pos; //esta en format matriu
+    std::queue<Pos> Q;
+    VVB visited(20, VB(47, false));
+    VVI distances(20, VI(47, 0));
+    Q.push(pos);
+    std::cerr << pos.x << " " << pos.y << std::endl; 
+}
+
+//CONSULTORAS
+
+
+void printItemMap(const std::map<std::string, itemInfo>& myMap) {
+    for (const auto& pair : myMap) {
+        std::cout << "Key: " << pair.first << std::endl;
+        std::cout << "Item Name: " << pair.second.itemName << std::endl;
+        std::cout << "Position: " << pair.second.pos.x << " " << pair.second.pos.y << std::endl;
+        std::cout << "Values: ";
+        for (int value : pair.second.pickingTimes) {
+            std::cout << value << " ";
+        }
+        std::cout << "\n\n"; // Separate entries with empty lines
+    }
+}
+
 int main() 
 {
-
+    
+    readBoard("./data/planogram_table.csv");
+    readArticleInfo("./data/hackathon_article_picking_time.csv");
+    newFill("a9068");
+    /*
+    printItemMap(itemInfoMap);
+    for(int i = 0; i < boardInfo.size(); ++i) {
+        for(int j = 0;  j < boardInfo[0].size(); ++j) {
+            if(boardInfo[i][j] == "" and passableBoard[i][j]) std::cout << "  ";
+            else if(not passableBoard[i][j]) std::cout << "X ";
+            else if(boardInfo[i][j] != "") std::cout << "- ";
+        }
+        std::cout << std::endl;
+    }*/
+    
+    
+    
 }
